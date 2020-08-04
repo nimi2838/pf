3a45a320ef127d61773b5d5f34824158a2493ddd
nimi2838 <bbongsky11@naver.com>
2020-08-04 21:05:21 +0900

    menu-bar:hover ul>li

--- part/ezwell/index.js
+++
@@ -1,8 +0,0 @@
-function SideBar__init() {
-    $('.side-bar > nav > ul > li').click(function() {
-      var $this = $(this);
-      var $ul = $this.find(' > ul');
-      $ul.slideToggle();
-    });
-  }
-  
